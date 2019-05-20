ad9de98fe1760cf912f47c120a0a4400655f28de
mdx919 <thanvirdx919@gmail.com>
2019-05-20 15:59:13 -0400

    added new dafault client

--- src/main/java/com/cognizant/apigateway/SimpleCORSFilter.java
+++ src/main/java/com/cognizant/apigateway/SimpleCORSFilter.java
@@ -1,4 +1,7 @@
 package com.cognizant.apigateway;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
